a605a087fdb3ad593dd6aec1930b23867fce124c
JustinRaz <justinrazmont@gmail.com>
2020-03-21 19:25:56 +0800

    faculty now connects to the right db

--- faculty.php
+++ faculty.php
@@ -1,6 +1,12 @@
 <?php 
 
-    
+    $server = "localhost";
+    $database = "id12993300_a_better_ismis";
+    $username = "id12993300_justinraz";
+    $password = "justinraz";
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+
 
 ?>
 
@@ -10,8 +16,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A Better ISMIS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hello</h1>
+
+    <h1></h1>
+
+    <li>View assigned subject</li>
+    <li>
+        <p>Shedule</p>
+        <ul>
+            <li>View students</li>
+        </ul>
+    </li>
+    <li></li>
 </body>
 </html>
