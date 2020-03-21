d0c46beb60ac70c1b3d689dd3112213b5d083678
JustinRaz <justinrazmont@gmail.com>
2020-03-21 20:16:24 +0800

    php include to ur file for db credentials

---
+++ connect_to_db.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $server = "localhost";
+    $database = "id12993300_a_better_ismis";
+    $username = "id12993300_justinraz";
+    $password = "justinraz";
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+
+
+?>
