9e7118c3a60b7873f802c34313cb4ccf1c04eb2e
JustinRaz <justinrazmont@gmail.com>
2020-03-21 20:55:17 +0800

    use this to connect to db

--- account.php
+++ account.php
@@ -3,12 +3,17 @@
     include 'connect_to_db.php';
 
     if(isset($_POST['submit_login'])){
-        
-        $res = $conn->query("SELECT * FROM students WHERE id=."$_POST['account_id']." && password='".$_POST['account_password']."'");
 
-        if(!$res->num_rows > 0){
-            
+        if($_POST['account_id'] == 'admin' && $_POST['account_password'] == 'admin'){
+            include 'admin.php';
+        }else{
+            $res = $conn->query("SELECT * FROM students WHERE id=."$_POST['account_id']." && password='".$_POST['account_password']."'");
+
+            if(!$res->num_rows > 0){
+                
+            }
         }
+        
 
     }
 
--- faculty.php
+++ faculty.php
@@ -1,12 +1,6 @@
 <?php 
 
-    $server = "localhost";
-    $database = "id12993300_a_better_ismis";
-    $username = "id12993300_justinraz";
-    $password = "justinraz";
-
-    $conn = mysqli_connect($server, $username, $password, $database);
-
+    include 'connect_to_db.php';
 
 ?>
 
