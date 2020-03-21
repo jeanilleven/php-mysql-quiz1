8201a71591110cf1dc3ce6344557082310305566
JustinRaz <justinrazmont@gmail.com>
2020-03-21 22:37:09 +0800

    check this our to know how login works

--- account.php
+++ account.php
@@ -5,15 +5,14 @@
     if(isset($_POST['submit_login'])){
 
         if($_POST['account_id'] == 'admin' && $_POST['account_password'] == 'admin'){
-            echo 'hello';   
-            include 'index.html';
+            include 'admin.php';
         }else{
             $account_query = "SELECT * FROM ".$_POST['account_type']." WHERE id=".$_POST['account_id']." AND password='".$_POST['account_password']."'";
             $res = $conn->query($account_query);
             $conn->close();
 
             echo $account_query;
-            
+
             if($res && $res->num_rows > 0){
                 include $_POST['account_type'].'.php';
             }else{
@@ -22,6 +21,4 @@
         }
 
     }
-
-
 ?>
