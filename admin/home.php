791261be04984fc6a6771ee084e36d15634cff0c
Jeanille Ven Abayon <jeanillevenabayon30@gmail.com>
2020-03-22 16:10:24 +0800

    renamed pages

--- admin/home.php
+++ admin/home.php
@@ -2,7 +2,6 @@
   require 'connect_to_db.php';
 ?>
 
-
 <!doctype html>
 <html>
     <head>
@@ -13,14 +12,13 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-        <script src="manage-enrollment-nav-js.js"></script>
         <link rel="stylesheet" href="index-styles.css" type="text/css">
     </head>
     <body>
       <nav id="header" style="background-color: #f8f9fa;">
         <img id="navbar-pic" height="50" src="pics/nav-logo.png" style="margin-left: 50px;">
         <span  style="color:#07500b; " class="dropdown">
-          <a style="box-shadow:none;"id="user-dropdown" class="btn btn-secondary dropdown-toggle" href="#" role="button" id="user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a style="box-shadow:none;" id="user-dropdown" class="btn btn-secondary dropdown-toggle" href="#" role="button" id="user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <img id="user-icon" style="border-radius: 50%; border: 1px solid gray;" src="pics/man-icon.png" alt="icon">
             <span style="color:#07500b; " id="user-id">18400175</span>
           </a>
@@ -40,7 +38,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a style="color:white;"class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+              <a style="color:white;"class="nav-link" href="admin.php">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
               <a style="color:white;" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -49,7 +47,7 @@
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="manage-faculty.php">Faculty</a>
                 <a class="dropdown-item" href="manage-students.php">Students</a>
-                <a class="dropdown-item" href="#">Rooms</a>
+                <a class="dropdown-item" href="manage-rooms.php">Rooms</a>
               </div>
             </li>
             <li class="nav-item dropdown">
@@ -57,9 +55,9 @@
                 Enrollment
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="enrollment-student.php">Student Enrollment</a>
-                <a class="dropdown-item" href="enrollment-subjects.php">Subjects</a>
-                <a class="dropdown-item" href="enrollment-subject-offerings.php">Subject-Offerings</a>
+                <a class="dropdown-item" href="#">Student Enrollment</a>
+                <a class="dropdown-item" href="#">Subjects</a>
+                <a class="dropdown-item" href="#">Subject-Offerings</a>
               </div>
             </li>
         </div>
@@ -70,7 +68,7 @@
 
       <!-- CHANGE PASSWORD MODAL -->
       <div class="modal fade" id="change-pw-modal" tabindex="-1" role="dialog" aria-labelledby="change-pw-moda" aria-hidden="true">
-        <div class="modal-dialog " role="document">
+        <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="">Change Password</h5>
@@ -109,74 +107,15 @@
       </div>
 
 
-      <div class="container" style="margin-top: 20px;">
-          <div class="row">
-            <div class="col-lg-6">
-                <h1>ROOMS</h1>
-            </div>
-            <div class="col-lg-6">
-                <button style="float:right;"type="button" class="btn btn-success" data-toggle="modal" data-target="#add-room-modal">
-                    Add Room
-                </button>
-                  
-                  <!-- Add Room Modal -->
-                  <div class="modal fade" id="add-room-modal" tabindex="-1" role="dialog" aria-labelledby="add-room-modal" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="add-room-modal">Add Room</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-                        <div class="modal-body">
-                        <form action="manage-rooms.php" method="get">
-                          <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                              <span class="input-group-text" id="name"><i class='fas fa-door-open'></i></span>
-                            </div>
-                            <input name="name" type="text" class="form-control" placeholder="Room Name" aria-label="name" aria-describedby="basic-addon1">
-                          </div>
-                          <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                              <span class="input-group-text" id="capacity"><i class='fas fa-users'></i></span>
-                            </div>
-                            <input name="capacity" min=0 type="number" class="form-control" placeholder="Capacity" aria-label="capacity" aria-describedby="basic-addon1">
-                          </div>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-success">Add</button>
-                        </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-          </div>
-      </div>
-
-      <!-- ADD ROOM -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-      
   </body>
      
 </html>
+
+<script>
+  $('#user-id').css("margin-bottom", "0.5rem");
+  $('#user-id').css("margin-top", "0.5rem");
+  $('#user-id').css("margin-left", "15px");
+
+  $('#user-dropdown').css("background-color", "#f8f9fa");
+  $('#user-dropdown').css("border", "none");
+</script>
--- admin/manage-students.php
+++ admin/manage-students.php
@@ -39,7 +39,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a style="color:white;"class="nav-link" href="admin.php">Home <span class="sr-only">(current)</span></a>
+              <a style="color:white;"class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
               <a style="color:white;" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
